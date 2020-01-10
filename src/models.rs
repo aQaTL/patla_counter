65d72ce21a33cde0e342aacad09327403e52965d
aQaTL <mmsoltys@outlook.com>
2020-01-10 23:38:00 +0100

    cargofmt'd

--- src/models.rs
+++ src/models.rs
@@ -1,23 +1,23 @@
 use super::schema::entries;
-use serde::Serialize;
 use diesel::{Insertable, Queryable};
+use serde::Serialize;
 
 #[derive(Serialize, Queryable)]
 pub struct Entry {
-    pub id: i32,
-    pub reason: Option<String>,
-    pub created: chrono::NaiveDateTime,
+	pub id: i32,
+	pub reason: Option<String>,
+	pub created: chrono::NaiveDateTime,
 }
 
 #[derive(Insertable)]
-#[table_name="entries"]
+#[table_name = "entries"]
 pub struct InsertEntry {
-    pub reason: Option<String>,
-    pub created: chrono::NaiveDateTime,
+	pub reason: Option<String>,
+	pub created: chrono::NaiveDateTime,
 }
 
 #[derive(Serialize, Queryable)]
 pub struct Password {
-    pub id: i32,
-    pub password: [char; 32],
+	pub id: i32,
+	pub password: [char; 32],
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,19 +1,16 @@
 table! {
-    entries (id) {
-        id -> Int4,
-        reason -> Nullable<Text>,
-        created -> Timestamp,
-    }
+	entries (id) {
+		id -> Int4,
+		reason -> Nullable<Text>,
+		created -> Timestamp,
+	}
 }
 
 table! {
-    passwords (id) {
-        id -> Int4,
-        password_hash -> Bpchar,
-    }
+	passwords (id) {
+		id -> Int4,
+		password_hash -> Bpchar,
+	}
 }
 
-allow_tables_to_appear_in_same_query!(
-    entries,
-    passwords,
-);
+allow_tables_to_appear_in_same_query!(entries, passwords,);
